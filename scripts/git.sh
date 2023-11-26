aa604b23c7c5acc151a07fc01b8c756a2d6b2e18
Meligy <Eng.Meligy@gmail.com>
2023-11-26 20:17:37 +1100

    Apply default git credential manager config

--- scripts/git.sh
+++ scripts/git.sh
@@ -9,6 +9,11 @@ brew install -q git-flow
 # Git Cedential Manager https://github.com/GitCredentialManager/git-credential-manager
 brew tap microsoft/git
 brew install -q --cask git-credential-manager-core
+# Apply it
+git config --global credential."https://dev.azure.com".azreposCredentialType oauth
+git config --global credential."https://dev.azure.com".useHttpPath true
+git config --global credential.helper ""
+git config --global credential.helper "/usr/local/share/gcm-core/git-credential-manager"
 
 # A git CLI alternative with Github specific features
 brew install -q hub
@@ -22,4 +27,4 @@ brew install --cask -q fork
 # config
 
 git config --global user.name "Meligy"
-git config --global user.email Eng.Meligy@gmail.com 
+git config --global user.email Eng.Meligy@gmail.com
